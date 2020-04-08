79f22272319c4dd7900c38a5b2df60c3707451a7
LauraFlaherty <g00348586@gmit.ie>
2020-04-08 20:16:12 +0100

    project finished

---
+++ Project/Assets/Script/CollisonDetection.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisonDetection : MonoBehaviour
+{
+    public GameObject Objects; 
+
+   private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject.name); 
+        /* foreach (Transform child in other.transform){
+            Debug.Log(child.name); 
+        }*/
+    }
+}
